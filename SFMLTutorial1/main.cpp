3cff9a2c0b53cd2c718af6ac9fffaa73ae1b1680
Richard Schwalk <rschwalk@outlook.com>
2016-05-26 17:58:16 +0200

    Move object with keybord

--- SFMLTutorial1/main.cpp
+++ SFMLTutorial1/main.cpp
@@ -11,21 +11,35 @@ int main()
 	rectangle.setOrigin(sf::Vector2f(25, 25));
 	rectangle.setPosition(sf::Vector2f(50, 50));
 
+	bool moving = false;
+
 	while (window.isOpen())
 	{
 		// Handle events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::EventType::Closed)
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
+			{
+				moving = true;
+			}
+			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Right) 
+			{
+				moving = false;
+			}
+			if ((event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Escape) || 
+				event.type == sf::Event::EventType::Closed)
 			{
 				window.close();
 			}
 		}
 
 		// Update scene
-		rectangle.rotate(1.5f);
-		rectangle.move(sf::Vector2f(1, 0));
+		if (moving)
+		{
+			rectangle.rotate(1.5f);
+			rectangle.move(sf::Vector2f(1, 0));
+		}
 
 		// Render cycle
 		window.clear(sf::Color::Black);
