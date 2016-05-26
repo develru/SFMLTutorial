a3031cbc36f2f69029073f89d5acbdd6d2e0f656
Richard Schwalk <rschwalk@outlook.com>
2016-05-26 18:06:43 +0200

    Simplified key event handling

--- SFMLTutorial1/main.cpp
+++ SFMLTutorial1/main.cpp
@@ -11,22 +11,12 @@ int main()
 	rectangle.setOrigin(sf::Vector2f(25, 25));
 	rectangle.setPosition(sf::Vector2f(50, 50));
 
-	bool moving = false;
-
 	while (window.isOpen())
 	{
 		// Handle events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
-			{
-				moving = true;
-			}
-			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Right) 
-			{
-				moving = false;
-			}
 			if ((event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Escape) || 
 				event.type == sf::Event::EventType::Closed)
 			{
@@ -35,7 +25,7 @@ int main()
 		}
 
 		// Update scene
-		if (moving)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
 		{
 			rectangle.rotate(1.5f);
 			rectangle.move(sf::Vector2f(1, 0));
